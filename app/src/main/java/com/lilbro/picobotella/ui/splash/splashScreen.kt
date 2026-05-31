5f8feacf9f8620785ec4dcbe9af3e1a094b627e6
Kewin <kealgri@gmail.com>
2026-05-31 15:05:00 -0500

    feat: Improved bottle render perfomance, added ModelCache.kt to preload model & now we're preloading in the splash screen so after the change the model is previously charged in ram.

--- app/src/main/java/com/lilbro/picobotella/ui/splash/splashScreen.kt
+++ app/src/main/java/com/lilbro/picobotella/ui/splash/splashScreen.kt
@@ -3,14 +3,17 @@ package com.lilbro.picobotella.ui.splash
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import com.google.android.filament.utils.Utils
 import com.lilbro.picobotella.R
 import com.lilbro.picobotella.ui.home.MainActivity
+import com.lilbro.picobotella.utils.ModelCache
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @SuppressLint("CustomSplashScreen")
 class SplashScreen : AppCompatActivity()
@@ -21,18 +24,25 @@ class SplashScreen : AppCompatActivity()
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash_screen)
 
+        // Initialize Filament as soon as possible
+        Utils.init()
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        // Handler to delay the screen for 5 seconds
-        Handler(Looper.getMainLooper()).postDelayed(
-            {
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
-                finish()
-            }, 5000)
+        lifecycleScope.launch {
+            // Preload the 3D model in background
+            ModelCache.preloadModel(this@SplashScreen)
+            
+            // Wait remaining time if needed (total 3 seconds instead of 5 to make it feel faster)
+            delay(3000)
+            
+            val intent = Intent(this@SplashScreen, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
