a0d24e7474ff5d0ef4aee7b9aef3458cd43fb436
Hoods05 <hoods14gang@gmail.com>
2026-06-24 17:52:52 -0500

    wip: guardar cambios locales antes de merge

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,18 +1,14 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.ksp)
     alias(libs.plugins.hilt)
     alias(libs.plugins.google.services)
-    kotlin("android")
 }
 
 android {
     namespace = "com.lilbro.picobotella"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.lilbro.picobotella"
